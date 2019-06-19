8d38dd0d9250f07367f591187301a0145a8c88c2
Ilya Radchenko <knownasilya@gmail.com>
2019-06-19 12:39:31 -0400

    feat: initial pass

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 'use strict';
 
+const BroccoliDebug = require('broccoli-debug');
+const path = require('path');
+const Plugin = require('./lib/plugin');
+
+const debugTree = BroccoliDebug.buildDebugCallback(`some-awesome`);
+
+
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  setupPreprocessorRegistry(type, registry) {
+    registry.add('js', {
+      name: 'ember-sfc',
+      ext: 'ember',
+      toTree: (tree) => {
+        let input = debugTree(tree, 'input');
+        let sfcTree = Plugin([input]);
+        let output = debugTree(sfcTree, 'output');
+        return output;
+      }
+    });
+
+    if (type === 'parent') {
+      this.parentRegistry = registry;
+    }
+  },
 };
